59089aa5f15775ffcedbb46a75870d2608d328d5
Guilherme Fortes <guilhermefortesoffice@gmail.com>
2025-05-09 00:08:46 -0300

    Início - Componente Note
    Início da implementação do componente Note
    
    Até agora:
    - Implementação do input e textarea (pretendo mudar esse textarea...)
    - Estilização

--- to-do list/src/views/Note.vue
+++ to-do list/src/views/Note.vue
@@ -1,19 +1,70 @@
 <script setup>
-
+import IconCheck from '../components/icons/IconCheck.vue'
 </script>
 
 
 <template>
-    <div>
-        
+    <div class="container">
+        <div>
+            <input v-model="title" placeholder="Título" class="note-title" />
+        </div>
+        <div>
+            <textarea v-model="content" placeholder="Conteúdo" class="note-content" />
+        </div>
+
+        <div class="div-iconcheck">
+            <IconCheck />
+        </div>
     </div>
 </template>
 
 
 <style scoped>
-div {
-    height: 100px;
-    width: 300px;
-    background: red;
+
+input,
+textarea {
+    color: white;
 }
+
+input {
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+textarea {
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+input::placeholder,
+textarea::placeholder {
+  font-family: sans-serif; 
+}
+
+.note-title {
+    width: 100vw;
+    height: 6vh;
+    margin-top: 1vh;
+    padding: 10px;
+    font-size: 30px;
+}
+
+.note-content {
+    margin-top: 1vh;
+    height: 83.2vh;
+    width: 100vw;
+    font-size: 18px;
+    padding: 5px;
+}
+
+.note-title, .note-content{
+    background: none;
+    border: none;
+    outline: 0;
+}
+
+.div-iconcheck {
+    width: 100vw;
+    background: rgba(0, 0, 0, 0.541); /* Só afeta a div */
+    height: 8vh;
+}
+
 </style>

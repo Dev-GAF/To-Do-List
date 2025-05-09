ee0c33ac1779f67adbddca5958cd79fd06422ccf
Guilherme Fortes <guilhermefortesoffice@gmail.com>
2025-05-09 00:05:31 -0300

    Icon - Check
    Fiz e me baseei em outros Icons (com svg), para fazer esse Check...

---
+++ to-do list/src/components/icons/IconCheck.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="icon-check">
+        <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="40" 
+        height="40" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        stroke="currentColor" 
+        stroke-width="2" 
+        stroke-linecap="round" 
+        stroke-linejoin="round"
+        >
+        <path d="M20 6L9 17l-5-5" />
+        </svg>
+    </div>
+</template>
+
+<style scoped>
+
+.icon-check {
+  position: fixed;
+  right: 20px;
+  bottom: 1vh;
+  color: rgb(13, 225, 207);
+  cursor: pointer;
+}
+
+</style>

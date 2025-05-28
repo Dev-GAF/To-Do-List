391a9b3f5c338f157270c1ecb9f7468df7daa9fb
Guilherme Fortes <guilhermefortesoffice@gmail.com>
2025-05-28 12:03:38 -0300

    Adiciona rotas REST para Checklists
    - Define rotas GET, POST, PUT e DELETE para /checklists
    - Integra os controladores de Checklist com o Echo
    - Agrupa rotas usando e.Group para melhor organização

---
+++ to-do list/src/server/routes/checklist_routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"server/controllers"
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterCheckListRoutes(e *echo.Echo) {
+	checklist := e.Group("/checklists")
+
+	checklist.GET("", controllers.ListarChecklists)           // GET /checklists
+	checklist.POST("", controllers.CriarChecklist)            // POST /checklists
+	checklist.PUT("/:id", controllers.AtualizarChecklist)     // PUT /checklists/:id
+	checklist.DELETE("/:id", controllers.RemoverChecklist)    // DELETE /checklists/:id
+}
